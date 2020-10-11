53002e092cb38023571752e07d8fb11ff43629c1
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-11 16:18:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,17 @@
 def reverse_each_word string
   string_array= string.split(/ /)
   
-  reverse_string= ""
+  #reverse_string= ""
   
   string_array.collect do |word| 
     
-   reverse_string << word.reverse + " "
+   #reverse_string << word.reverse + " "
+   
+   string_array.join()
    
   end
  
-  reverse_string.chop
+  #reverse_string.chop
  
 end 
 
