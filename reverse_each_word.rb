7f5d6d434fcea56073693a20a342ea4f01c02629
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-11 16:17:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,29 @@
+def reverse_each_word string
+  string_array= string.split(/ /)
+  
+  reverse_string= ""
+  
+  string_array.collect do |word| 
+    
+   reverse_string << word.reverse + " "
+   
+  end
+ 
+  reverse_string.chop
+ 
+end 
+
+
+
+#def reverse_each_word string
+  
+  
+ # string_array= string.split
+  
+ # reverse_string = " "
+  
+ # string_array.collect { |word| reverse_string << word.reverse + " ")}
+  
+# reverse_string.chop
+  
+#end 
